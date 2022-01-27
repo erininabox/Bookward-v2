2a4fa1342946905d550b5095dc7e2b23bc898f03
erininabox <erininabox@gmail.com>
2022-01-26 19:34:43 -0800

    controllers

--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const models = require('../models');
+const models = require('../../models');
 
 router.get('/', (req, res) => {
     models.bookSet.find({}, (err, foundBooks) => {
