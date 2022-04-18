fa6ef2b605d43b5d5ac1eb2153896bb3aff728c9
erininabox <erininabox@gmail.com>
2022-04-17 17:12:23 -0700

    tried a fix for broken logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import {Link} from 'react-router-dom';
+const BookWard = require('./logos/BookWard.png')
 
 function Header(){
     return(
         <div className="header">
-            <img src="https://i.ibb.co/8YXzs7p/BookWard.png" alt="BookWard" border="0" className="logo" />
+            <img src={BookWard} alt="BookWard" border="0" className="logo" />
             <nav className="nav">
             <Link className="link" to='/'>Bookward</Link>
             <Link className="link" to='/books'>Books</Link>
